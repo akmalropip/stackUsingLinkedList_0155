407be31288e34a400bdbbdd80b5c2cec329580a0
akmalropip <akmalropip@gmail.com>
2026-05-15 15:26:55 +0700

    default

--- stack.cpp
+++ stack.cpp
@@ -109,3 +109,7 @@ int main()
             case 4:
             cout << "Exiting program." << endl;
             break;
+            default:
+            cout << "Invalid choice. Try again." << endl;
+            break;
+       
